1e2344ec1f593c3ca508fe33ce5e26b55c2e8b17
munkur <munkur@autocapcut.live>
2026-07-09 06:00:05 +0000

    File changes

--- src/components/CollectionsSection.jsx
+++ src/components/CollectionsSection.jsx
@@ -4,7 +4,7 @@ import CategoryCard from "@/components/CategoryCard";
 
 const CATEGORIES = [
   { name: "L-Shape Sofas", image: "https://media.base44.com/images/public/6a4f37503fd1b5d00fb76938/3dd5fa506_IMG-20240619-WA0001.jpg" },
-  { name: "3+1+1 Sofas", image: "https://media.base44.com/images/public/6a45ebcde6fb4264c098459a/33ab3b057_generated_ebfd2241.png" },
+  { name: "3+1+1 Sofas", image: "https://media.base44.com/images/public/6a4f37503fd1b5d00fb76938/77d4901a4_IMG-20241224-WA0001.jpg" },
   { name: "Dining", image: "https://media.base44.com/images/public/6a45ebcde6fb4264c098459a/28241d3fd_generated_a5f70759.png" },
   { name: "Cots", image: "https://media.base44.com/images/public/6a45ebcde6fb4264c098459a/08086edb0_generated_a8b573f5.png" },
   { name: "Recliner Sofas", image: "https://media.base44.com/images/public/6a45ebcde6fb4264c098459a/c71623eae_generated_e570ac4c.png" },
